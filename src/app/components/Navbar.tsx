b13ee5522b759a9079c6dd2af493725b8bf8b554
Evan Servito <78441979+evanpservito@users.noreply.github.com>
2025-03-06 22:53:10 -0800

    add assets and back button

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { motion } from "motion/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
+  const pathname = usePathname();
   const navLinks = [
     { title: "Projects", path: "/#projects" },
     { title: "Experience", path: "/#experience" },
@@ -19,7 +21,7 @@ const Navbar = () => {
       <div className="hidden md:flex flex-row justify-between fixed top-8 right-8 left-8">
         <Link href={"/"}>
           <p className="font-DMSans font-bold text-purple transition hover:scale-110 hover:duration-250 hover:text-white">
-            [ Evan Servito ]
+            {pathname === "/" ? "[ Evan Servito ]" : "[ Return Home ]"}
           </p>
         </Link>
         <div className="flex flex-row space-x-8 font-KodeMono">
@@ -36,7 +38,7 @@ const Navbar = () => {
       <div className="md:hidden flex flex-row justify-between fixed top-8 right-8 left-8">
         <Link href={"/"}>
           <p className="font-DMSans font-bold text-purple transition hover:scale-110 hover:duration-250 hover:text-white">
-            [ ES ]
+            {pathname === "/" ? "[ ES ]" : "[ Return Home ]"}
           </p>
         </Link>
         <RxHamburgerMenu size={21} />
--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,5 +1,6 @@
 import { motion } from "motion/react";
 import React from "react";
+import Image from "next/image";
 
 interface ProjectProps {
   title: string;
@@ -8,9 +9,18 @@ interface ProjectProps {
 }
 const Project = ({ title, desc, imgPath }: ProjectProps) => {
   return (
-    <div className="border-2 px-4 py-12 lg:p-4 w-full lg:w-4/5 transition hover:scale-110 hover:border-purple hover:duration-250 rounded-xl">
-      <p className="font-Rubik font-bold text-xl lg:text-lg">{title}</p>
-      <p className="hidden lg:flex font-KodeMono text-grey ">[ {desc} ]</p>
+    <div className="relative border-2 px-4 py-12 lg:py-8 w-full transition hover:scale-110 hover:border-purple hover:duration-250 rounded-xl flex flex-row overflow-hidden">
+      <div>
+        <p className="font-Rubik font-bold text-xl lg:text-lg">{title}</p>
+        <p className="hidden lg:flex font-KodeMono text-grey ">[ {desc} ]</p>
+      </div>
+      <Image
+        src={imgPath}
+        width={200}
+        height={200}
+        alt={title}
+        className="opacity-50 absolute right-4 -top-8 "
+      />
     </div>
   );
 };
